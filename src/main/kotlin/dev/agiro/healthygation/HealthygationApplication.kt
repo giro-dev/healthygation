09ef3c4b0fca235589be55043805f83fce6e5536
ALBERT GIRO QUER <albert.giro_quer@mercedes-benz.com>
2026-07-28 23:05:32 +0200

    init

---
+++ src/main/kotlin/dev/agiro/healthygation/HealthygationApplication.kt
@@ -0,0 +1,11 @@
+package dev.agiro.healthygation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HealthygationApplication
+
+fun main(args: Array<String>) {
+	runApplication<HealthygationApplication>(*args)
+}
